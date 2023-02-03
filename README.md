aa52cd096f4390351f7dcfd919010a1b31dbdcaa
Stefano Pigozzi <me@steffo.eu>
2023-02-03 17:14:31 +0100

    Update to version 0.2.0

---
+++ README.md
@@ -0,0 +1,5 @@
+# youtubedlez
+
+A super simple interface for [`yt-dlp`](https://github.com/yt-dlp/yt-dlp)
+
+
--- youtubedlez/__main__.py
+++ youtubedlez/__main__.py
@@ -1,59 +1,66 @@
-import youtube_dl
+import yt_dlp
 import pathlib
-import datetime
 import os
+import pkg_resources
+
+def main():
+    __version__ = pkg_resources.get_distribution("youtubedlez")
+
+    print(f"Easy Youtube Downloader {__version__}")
+    print("(c) Stefano Pigozzi")
+    print("Released under the GPL 3.0")
+    print("-" * 80)
+    print()
+
+    dl_url = input("What video(s) do you want to download? ")
+
+    while True:
+        while True:
+            dl_format = input("What format to you want the downloaded file(s) to have? ")
+            video_formats = ['mp4', 'flv', 'webm', 'ogg', 'mkv', 'avi']
+            audio_formats = ['best', 'aac', 'flac', 'mp3', 'm4a', 'opus', 'vorbis', 'wav']
+
+            if dl_format in video_formats:
+                postprocessors = [
+                    {
+                        'key': 'FFmpegVideoConvertor',
+                        'preferedformat': dl_format,
+                    },
+                ]
+                break
+            elif dl_format in audio_formats:
+                postprocessors = [
+                    {
+                        'key': 'FFmpegExtractAudio',
+                        'preferredcodec': dl_format,
+                    },
+                ]
+                break
+            else:
+                print("Invalid format specified")
+                print("Formats available: ")
+                print("Video: " + " ".join(video_formats))
+                print("Audio: " + " ".join(audio_formats))
 
 
-print(f"Easy Youtube Downloader")
-print(f"(c) {datetime.datetime.now().year} Stefano Pigozzi")
-print(f"Released under the GPL 3.0")
-print("-" * 80)
-print()
+        dl_location = pathlib.Path.home().joinpath("Downloads")
+        os.makedirs(name=dl_location, exist_ok=True)
+        print(f"The downloads will be saved in: {dl_location}")
 
-dl_url = input("What video(s) do you want to download? ")
+        print()
+        with yt_dlp.YoutubeDL({
+            "noplaylist": True,
+            "prefer_ffmpeg": True,
+            "outtmpl": f"{dl_location}/%(extractor)s__%(title)s.%(ext)s",
+            "postprocessors": postprocessors,
+            "addmetadata": True,
+        }) as ytdl:
+            ytdl.download([dl_url])
+        print()
 
-while True:
-    dl_format = input("What format to you want the downloaded file(s) to have? ")
-    video_formats = ['mp4', 'flv', 'webm', 'ogg', 'mkv', 'avi']
-    audio_formats = ['best', 'aac', 'flac', 'mp3', 'm4a', 'opus', 'vorbis', 'wav']
-
-    if dl_format in video_formats:
-        postprocessors = [
-            {
-                'key': 'FFmpegVideoConvertor',
-                'preferedformat': dl_format,
-            },
-        ]
-        break
-    elif dl_format in audio_formats:
-        postprocessors = [
-            {
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': dl_format,
-            },
-        ]
-        break
-    else:
-        print("Invalid format specified")
-        print("Formats available: ")
-        print("Video: " + " ".join(video_formats))
-        print("Audio: " + " ".join(audio_formats))
+        print("Download complete!")
+        print("-" * 80)
 
 
-dl_location = pathlib.Path.home().joinpath("Downloads")
-os.makedirs(name=dl_location, exist_ok=True)
-print(f"The downloads will be saved in: {dl_location}")
-
-print()
-with youtube_dl.YoutubeDL({
-    "noplaylist": True,
-    "prefer_ffmpeg": True,
-    "outtmpl": f"{dl_location}/%(extractor)s__%(title)s.%(ext)s",
-    "postprocessors": postprocessors,
-}) as ytdl:
-    ytdl.download([dl_url])
-print()
-
-print("Download complete!")
-print("Press any key to exit.")
-input()
+if __name__ == "__main__":
+    main()
