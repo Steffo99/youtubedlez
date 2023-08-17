f671f0087b22f43f273c6603b88a542f3acd405f
Stefano Pigozzi <me@steffo.eu>
2023-08-17 19:28:28 +0200

    Update to 0.3.0

--- youtubedlez/__main__.py
+++ youtubedlez/__main__.py
@@ -1,65 +1,89 @@
 import yt_dlp
 import pathlib
 import os
-import pkg_resources
+
+video_formats = ['mp4', 'flv', 'webm', 'ogg', 'mkv', 'avi']
+audio_formats = ['best', 'aac', 'flac', 'mp3', 'm4a', 'opus', 'vorbis', 'wav']
 
 def main():
-    __version__ = pkg_resources.get_distribution("youtubedlez")
-
-    print(f"Easy Youtube Downloader {__version__}")
+    print(f"Easy Youtube Downloader")
     print("(c) Stefano Pigozzi")
-    print("Released under the GPL 3.0")
+    print("Licensed under the GNU-GPL-3.0-or-later")
     print("-" * 80)
     print()
 
-    dl_url = input("What video(s) do you want to download? ")
+    dl_location = pathlib.Path.home().joinpath("Downloads")
+    os.makedirs(name=dl_location, exist_ok=True)
+    print(f"Downloads will be saved in: {dl_location}")
+    print("-" * 80)
+    print()
 
+    arguments = {
+        "noplaylist": True,
+        "prefer_ffmpeg": True,
+        "restrictfilenames": True,
+        "windowsfilenames": True,
+    }
     while True:
-        while True:
-            dl_format = input("What format to you want the downloaded file(s) to have? ")
-            video_formats = ['mp4', 'flv', 'webm', 'ogg', 'mkv', 'avi']
-            audio_formats = ['best', 'aac', 'flac', 'mp3', 'm4a', 'opus', 'vorbis', 'wav']
+        dl_format = input("What format to you want the downloaded file(s) to have? ")
 
-            if dl_format in video_formats:
-                postprocessors = [
+        if dl_format in video_formats:
+            arguments |= {
+                "format": "best",
+                "postprocessors": [
                     {
                         'key': 'FFmpegVideoConvertor',
                         'preferedformat': dl_format,
                     },
-                ]
-                break
-            elif dl_format in audio_formats:
-                postprocessors = [
+                ],
+            }
+            break
+        elif dl_format in audio_formats:
+            arguments |= {
+                "format": "bestaudio/best",
+                "postprocessors": [
                     {
                         'key': 'FFmpegExtractAudio',
                         'preferredcodec': dl_format,
                     },
-                ]
-                break
-            else:
-                print("Invalid format specified")
-                print("Formats available: ")
-                print("Video: " + " ".join(video_formats))
-                print("Audio: " + " ".join(audio_formats))
+                    {
+                        'key': 'FFmpegMetadata',
+                        'add_metadata': True,
+                        'add_chapters': False,
+                        'add_infojson': False,
+                    },
+                ],
+            }
+            break
+        else:
+            print("Invalid format specified")
+            print("Formats available: ")
+            print("Video: " + " ".join(video_formats))
+            print("Audio: " + " ".join(audio_formats))
+            print()
+            continue
+    print("-" * 80)
+    print()
 
-
-        dl_location = pathlib.Path.home().joinpath("Downloads")
-        os.makedirs(name=dl_location, exist_ok=True)
-        print(f"The downloads will be saved in: {dl_location}")
-
-        print()
-        with yt_dlp.YoutubeDL({
-            "noplaylist": True,
-            "prefer_ffmpeg": True,
-            "outtmpl": f"{dl_location}/%(extractor)s__%(title)s.%(ext)s",
-            "postprocessors": postprocessors,
-            "addmetadata": True,
-        }) as ytdl:
-            ytdl.download([dl_url])
-        print()
-
-        print("Download complete!")
-        print("-" * 80)
+    while True:
+        try:
+            dl_url = input("What video(s) do you want to download? ")
+            with yt_dlp.YoutubeDL({
+                "outtmpl": f"{dl_location}/%(extractor)s__%(title)s.%(ext)s",
+                **arguments,
+                "postprocessors": [*arguments.get("postprocessors", [])],
+            }) as ytdl:
+                ytdl.download([dl_url])
+        except KeyboardInterrupt:
+            print("Exiting...")
+            break
+        except Exception:
+            print("Download error.")
+        else:
+            print("Download complete!")
+        finally:
+            print("-" * 80)
+            print()
 
 
 if __name__ == "__main__":
